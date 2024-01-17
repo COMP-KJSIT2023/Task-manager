7f4e2aea3648a1e6096b3ded0e4cd2aeae54330a
unknown <hd1@somaiya.edu>
2024-01-17 16:32:39 +0530

    forms

--- main/views.py
+++ main/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from .models import Todolist, Item
 from .forms import CreateNewList
 
@@ -12,5 +12,13 @@ def index(response, id):
     return render(response, "main/list.html", {"ls":ls})
 
 def create(response):
-    form = CreateNewList()
+    if response.method == "POST":
+        form = CreateNewList(response.POST)
+        if form.is_valid():
+            n= form.cleaned_data["Name"]
+            t= Todolist(name=n)
+            t.save()
+        return HttpResponseRedirect(f"/{t.id}")
+    else:
+        form = CreateNewList()
     return render(response, "main/create.html", {"form":form})
